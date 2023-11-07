b74004682f02f1095cb93b2f258c8146f4aa38b0
aldinoanggawan <aldinoanggawan@Anggas-MacBook-Pro.local>
2023-11-07 16:24:44 +0800

    create color constants

---
+++ src/shared/colors.ts
@@ -0,0 +1,8 @@
+export const FONT = 'rgb(33, 174, 149)';
+export const HIT_TARGET = 'rgb(255, 78, 81)';
+export const MISS_TARGET = 'rgb(65, 65, 65)';
+export const BOARD_BACKGROUND = 'rgb(247, 247, 247)';
+
+export const GRAY = 'rgb(155, 154, 154)';
+export const ORANGE = 'rgb(255, 167, 0)';
+export const GREEN = 'rgb(33, 174, 149)';
