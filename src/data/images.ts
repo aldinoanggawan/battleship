7791a84994dde20c15b87cdd0d475bae177c9b03
aldinoanggawan <aldinoanggawan@Anggas-MacBook-Pro.local>
2023-11-08 16:42:24 +0800

    map images

---
+++ src/data/images.ts
@@ -0,0 +1,14 @@
+export const shipImages = {
+  carrier: '/assets/carrier.png',
+  battleship: '/assets/battleship.png',
+  cruiser: '/assets/cruiser.png',
+  submarine: '/assets/submarine.png',
+  destroyer: '/assets/destroyer.png',
+};
+
+export const images = {
+  missSmall: '/assets/miss-small.png',
+  miss: '/assets/miss.png',
+  hitSmall: '/assets/hit-small.png',
+  hit: '/assets/hit.png',
+};
