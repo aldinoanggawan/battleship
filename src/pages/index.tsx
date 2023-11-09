4829050190bf5030ae19905b1e1d33d1bf5983fa
aldinoanggawan <aldinoanggawan@Anggas-MacBook-Pro.local>
2023-11-09 22:57:54 +0800

    add score count

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ const initialBoard: string[][] = Array.from({ length: 10 }, () =>
 
 const Home = () => {
   const [board, setBoard] = useState<string[][]>(initialBoard);
+  const [score1, setScore1] = useState(0);
 
   const onClickCell = ({ i, j }: { i: number; j: number }): void => {
     // if the cell has been selected do an early return
@@ -22,18 +23,26 @@ const Home = () => {
       ship.positions.some((position) => position[0] === i && position[1] === j)
     );
 
+    // if there's a ship at the selected cell
+    if (hitShipIndex !== -1) {
+      // update score
+      setScore1((prevScore) => prevScore + 1);
+    }
+
     const newBoard = [...board];
     // assign X for hit and O for miss
     newBoard[i][j] = hitShipIndex !== -1 ? 'X' : 'O';
     setBoard(newBoard);
   };
 
+  const formattedScore1 = score1.toString().padStart(2, '0');
+
   return (
     <Container>
       <Content>
         <BattleshipBoard board={board} onClickCell={onClickCell} />
         <ShipScoreWrapper>
-          <ScoreBoard score1='00' />
+          <ScoreBoard score1={formattedScore1} />
           <ShipList shipTypes={layoutData.shipTypes} />
         </ShipScoreWrapper>
       </Content>
