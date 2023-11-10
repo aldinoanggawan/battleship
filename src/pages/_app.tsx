3c3fabf5307744b0cd766305770083ca539c93a1
aldinoanggawan <aldinoanggawan@Anggas-MacBook-Pro.local>
2023-11-10 16:26:39 +0800

    add favicon and title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,31 @@
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 import { globalStyles } from '../shared/styles';
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
+    <Head>
+      <title>Battleship</title>
+      <link
+        rel='apple-touch-icon'
+        type='image/png'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
     {globalStyles}
     <Component {...pageProps} />
   </>
