955bff8284dd0294df16b7bc5b448922aa33a11b
aldinoanggawan <aldinoanggawan@Anggas-MacBook-Pro.local>
2023-11-10 15:29:31 +0800

    handle game over and restart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,56 @@
 import { useState } from 'react';
 import { BattleshipBoard } from '../components/BattleshipBoard/component';
-import { Container, Content, ShipScoreWrapper } from './styles';
+import {
+  Button,
+  Container,
+  Content,
+  GameOverWrapper,
+  ShipScoreWrapper,
+  Text,
+} from './styles';
 import { ScoreBoard } from '../components/ScoreBoard/component';
 import { ShipList } from '../components/ShipList/component';
 import { layoutData } from '../data/layout';
 import { HitCounts } from './types';
 
 // create a 2D array of 10x10 that's initially null
-const initialBoard: string[][] = Array.from({ length: 10 }, () =>
-  Array.from({ length: 10 }, () => null)
-);
+const getInitialBoard = (): string[][] => {
+  return Array.from({ length: 10 }, () =>
+    Array.from({ length: 10 }, () => null)
+  );
+};
+
+// create a deep copy of layoutData.layout
+const getInitialLayout = (): typeof layoutData.layout => {
+  return JSON.parse(JSON.stringify(layoutData.layout));
+};
+
+const initialHitCounts: HitCounts = {
+  carrier: 0,
+  battleship: 0,
+  cruiser: 0,
+  submarine: 0,
+  destroyer: 0,
+};
 
 const Home = () => {
-  const [board, setBoard] = useState<string[][]>(initialBoard);
+  const [board, setBoard] = useState<string[][]>(getInitialBoard());
   const [score1, setScore1] = useState<number>(0);
-  const [hitCounts, setHitCounts] = useState<HitCounts>({
-    carrier: 0,
-    battleship: 0,
-    cruiser: 0,
-    submarine: 0,
-    destroyer: 0,
-  });
+  const [hitCounts, setHitCounts] = useState<HitCounts>(initialHitCounts);
+  const [layout, setLayout] = useState(getInitialLayout());
 
   const onClickCell = ({ i, j }: { i: number; j: number }): void => {
     // if the cell has been selected do an early return
     if (board[i][j]) return;
 
     // check if there's a ship at the selected cell
-    const hitShipIndex = layoutData.layout.findIndex((ship) =>
+    const hitShipIndex = layout.findIndex((ship) =>
       ship.positions.some((position) => position[0] === i && position[1] === j)
     );
 
     // if there's a ship at the selected cell
     if (hitShipIndex !== -1) {
-      const hitShip = layoutData.layout[hitShipIndex];
+      const hitShip = layout[hitShipIndex];
 
       // update score
       setScore1((prevScore) => prevScore + 1);
@@ -43,25 +60,60 @@ const Home = () => {
         ...prevHitCounts,
         [hitShip.ship]: prevHitCounts[hitShip.ship] + 1,
       }));
+
+      // update layout
+      setLayout((prevLayout) => {
+        const newLayout = [...prevLayout];
+
+        // remove the hit position from the ship
+        const newHitShip = newLayout[hitShipIndex];
+        newHitShip.positions = newHitShip.positions.filter(
+          (position) => !(position[0] === i && position[1] === j)
+        );
+
+        // remove the ship from layout if it's sunk
+        if (newHitShip.positions.length === 0) {
+          newLayout.splice(hitShipIndex, 1);
+        }
+
+        return newLayout;
+      });
     }
 
-    const newBoard = [...board];
-    // assign X for hit and O for miss
-    newBoard[i][j] = hitShipIndex !== -1 ? 'X' : 'O';
-    setBoard(newBoard);
+    // update board
+    setBoard((prevBoard) => {
+      const newBoard = [...prevBoard];
+      // assign X for hit and O for miss
+      newBoard[i][j] = hitShipIndex !== -1 ? 'X' : 'O';
+      return newBoard;
+    });
+  };
+
+  const onRestart = (): void => {
+    setLayout(getInitialLayout());
+    setBoard(getInitialBoard());
+    setScore1(0);
+    setHitCounts(initialHitCounts);
   };
 
   const formattedScore1 = score1.toString().padStart(2, '0');
 
   return (
     <Container>
-      <Content>
-        <BattleshipBoard board={board} onClickCell={onClickCell} />
-        <ShipScoreWrapper>
-          <ScoreBoard score1={formattedScore1} />
-          <ShipList shipTypes={layoutData.shipTypes} hitCounts={hitCounts} />
-        </ShipScoreWrapper>
-      </Content>
+      {layout.length === 0 ? (
+        <GameOverWrapper>
+          <Text>Game over</Text>
+          <Button onClick={onRestart}>Restart</Button>
+        </GameOverWrapper>
+      ) : (
+        <Content>
+          <BattleshipBoard board={board} onClickCell={onClickCell} />
+          <ShipScoreWrapper>
+            <ScoreBoard score1={formattedScore1} />
+            <ShipList shipTypes={layoutData.shipTypes} hitCounts={hitCounts} />
+          </ShipScoreWrapper>
+        </Content>
+      )}
     </Container>
   );
 };
--- src/pages/styles.ts
+++ src/pages/styles.ts
@@ -1,8 +1,6 @@
 import styled from '@emotion/styled';
-import {
-  BREAKPOINT_DESKTOP,
-  BREAKPOINT_TABLET,
-} from '../shared/constants';
+import { BREAKPOINT_DESKTOP, BREAKPOINT_TABLET } from '../shared/constants';
+import { ORANGE } from '../shared/colors';
 
 export const Container = styled.div({
   minHeight: '100vh',
@@ -11,7 +9,7 @@ export const Container = styled.div({
   [`@media (min-width: ${BREAKPOINT_TABLET}px)`]: {
     padding: 16,
   },
-  
+
   [`@media (min-width: ${BREAKPOINT_DESKTOP}px)`]: {
     padding: 24,
   },
@@ -25,7 +23,6 @@ export const Content = styled.div({
     display: 'flex',
     flexDirection: 'row-reverse',
   },
-
 });
 
 export const ShipScoreWrapper = styled.div({
@@ -43,5 +40,34 @@ export const ShipScoreWrapper = styled.div({
     marginTop: 0,
     marginRight: 16,
     flex: 1,
+  },
+});
+
+export const GameOverWrapper = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  height: '100vh',
+});
+
+export const Text = styled.p({
+  fontSize: 24,
+  fontWeight: 'bold',
+  textAlign: 'center',
+  margin: '0 0 24px',
+});
+
+export const Button = styled.button({
+  backgroundColor: ORANGE,
+  border: 'none',
+  borderRadius: 4,
+  padding: '16px 32px',
+  fontSize: 16,
+  fontWeight: 'bold',
+  cursor: 'pointer',
+
+  '&:hover': {
+    opacity: 0.8,
   },
 });
